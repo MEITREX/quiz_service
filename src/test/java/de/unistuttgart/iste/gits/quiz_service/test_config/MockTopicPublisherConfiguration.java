2a4f3a855196ef470e656b759353aff53c6d63c0
Paul Bredl <34026908+PaulBredl@users.noreply.github.com>
2023-09-22 10:39:30 +0200

    23 backend assessment feedback (#38)
    * add feedback for logging quiz completion
    
    * remove print statement
    
    * update api doc

---
+++ src/test/java/de/unistuttgart/iste/gits/quiz_service/test_config/MockTopicPublisherConfiguration.java
@@ -0,0 +1,22 @@
+package de.unistuttgart.iste.gits.quiz_service.test_config;
+
+import de.unistuttgart.iste.gits.quiz_service.dapr.TopicPublisher;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+
+@TestConfiguration
+public class MockTopicPublisherConfiguration {
+
+    @Primary
+    @Bean
+    public TopicPublisher getTestTopicPublisher() {
+        TopicPublisher mockPublisher = Mockito.mock(TopicPublisher.class);
+        doNothing().when(mockPublisher).notifyUserWorkedOnContent(any());
+        return mockPublisher;
+    }
+}
